975fae327aedde2d4f91d9255d1feeb95787aad1
Mark Harvey <crimson.falconer@gmail.com>
2019-07-05 22:39:44 +1000

    Remove debug csv, add Categories as per Anomally's specs

--- LibraryExporterExpanded/LibraryExporterExpanded.ps1
+++ LibraryExporterExpanded/LibraryExporterExpanded.ps1
@@ -4,14 +4,11 @@
     $path = $PlayniteApi.Dialogs.SaveFile("CSV|*.csv|Formated TXT|*.txt")
     $platforms = $PlayniteApi.Database.GetPlatforms()
 
-    $platforms | Export-Csv -Path "C:\temp\plats.csv"
-
     $platformHash = @{}
     $platforms | ForEach-Object {$platformHash.Add($_.Id, $_.Name)}
 
-    if ($path)
-    {
-        $games = $games | Select Name, Source, ReleaseDate, Playtime, PlayCount, IsInstalled, Favorite, GameId, PlatformId, @{Name = "Platform"; Expression = {$platformHash[$_.PlatformId]}} | Export-Csv -NoTypeInformation -Path $path
+    if ($path) {
+        $games = $games | Select Name, Source, CompletionStatus, @{Name = "Categories"; Expression = {$_.Categories -join ";"}}, @{Name = "Platform"; Expression = {$platformHash[$_.PlatformId]}} | Export-Csv -NoTypeInformation -Path $path
         $PlayniteApi.Dialogs.ShowMessage("Library exported successfully.");
     }
 }
