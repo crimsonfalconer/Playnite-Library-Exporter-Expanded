4ee89d40a2589986e4054f043700cf00b31440e1
Mark Harvey <crimson.falconer@gmail.com>
2019-07-05 21:59:18 +1000

    Upload script

---
+++ LibraryExporterExpanded/LibraryExporterExpanded.ps1
@@ -0,0 +1,17 @@
+﻿function global:ExportLibrary()
+{
+    $games = $PlayniteApi.Database.GetGames()
+    $path = $PlayniteApi.Dialogs.SaveFile("CSV|*.csv|Formated TXT|*.txt")
+    $platforms = $PlayniteApi.Database.GetPlatforms()
+
+    $platforms | Export-Csv -Path "C:\temp\plats.csv"
+
+    $platformHash = @{}
+    $platforms | ForEach-Object {$platformHash.Add($_.Id, $_.Name)}
+
+    if ($path)
+    {
+        $games = $games | Select Name, Source, ReleaseDate, Playtime, PlayCount, IsInstalled, Favorite, GameId, PlatformId, @{Name = "Platform"; Expression = {$platformHash[$_.PlatformId]}} | Export-Csv -NoTypeInformation -Path $path
+        $PlayniteApi.Dialogs.ShowMessage("Library exported successfully.");
+    }
+}
